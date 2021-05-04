80e5cf4df7ebdfdb831c8071a922686dc23d58c3
AriakanYusupov <83660660+AriakanYusupov@users.noreply.github.com>
2021-05-04 17:54:59 +0200

    Inicio
    Creación Repositorio

---
+++ MainGame.java
@@ -0,0 +1,12 @@
+package com.spacegame;
+
+import com.badlogic.gdx.Game;
+
+public class MainGame extends Game {
+
+	@Override
+	public void create (){
+	setScreen(new Box2DPantalla(this));
+	}
+
+}
